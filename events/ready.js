07a2a88ab95edb35201002456dba2d377f2e0dc3
VangHockBey8242 <95428838+VangHockBey8242@users.noreply.github.com>
2022-01-27 15:39:27 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,19 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+ setInterval(function() {
+}, 8000);
+  var msgArray = [
+"!yardım Gece Development"
+ ];
+
+ setInterval(() => {
+  var rastgeleOyun = Math.floor(Math.random() * msgArray.length);
+  client.user.setActivity(`${msgArray[rastgeleOyun]}`)
+}, 5000);
+    console.log(`Ringo başarıyla giriş yaptı.`);
+}
